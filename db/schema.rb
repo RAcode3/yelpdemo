019a95bd9b8995b45b84679b0828be24e4ffdc11
Raphael Adamek <raphadamek@gmail.com>
2016-02-18 21:55:03 -0500

    Link reviews to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160205020342) do
+ActiveRecord::Schema.define(version: 20160219022455) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20160205020342) do
     t.string   "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
